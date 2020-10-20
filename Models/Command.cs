e92e0889b855d17f52b49c3b49d930d33200b5d4
LSC <lelandscrawford@gmail.com>
2020-10-20 18:09:54 -0400

    First commit

---
+++ Models/Command.cs
@@ -0,0 +1,10 @@
+namespace CommandFinder.Models
+{
+    public class CommandFinder
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platfrom { get; set; }
+    }
+}
